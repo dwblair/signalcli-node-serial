91a9dbca135ded3547994fe4484a9918de086a8d
Don Blair <donblair@gmail.com>
2021-02-19 20:43:57 -0500

    update

---
+++ arduino/heltec_serialize_screen_struct_rad/heltec_serialize_screen_struct_rad.ino
@@ -0,0 +1,141 @@
+#include <ArduinoJson.h>
+#include <U8x8lib.h>
+#include <SPI.h>
+#include <RH_RF95.h>
+
+// heltec wifi lora 32 v2
+#define RFM95_CS 18
+#define RFM95_RST 14
+#define RFM95_INT 26
+
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+char abbrev_message[10];
+
+typedef struct {
+  char sender[13];
+  char message[100]; 
+  int timestamp;
+} Payload;
+Payload theData;
+
+U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 15, /* data=*/ 4, /* reset=*/ 16);
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+
+u8x8.begin();
+  
+  u8x8.setFont(u8x8_font_7x14B_1x2_f);
+   u8x8.clear();
+   u8x8.setCursor(0,0); 
+   u8x8.print("Starting...");
+   delay(1000);
+
+   if (!setupLoRa()) {
+    Serial.println("LoRa setup failed ... ");
+     u8x8.setCursor(0,4); 
+   u8x8.print("LoRa not set?");
+    while (1)
+      ;
+   }
+    u8x8.setCursor(0,4); 
+   u8x8.print("LoRa setup!");
+   delay(1000);
+   
+}
+
+void loop() {
+
+  if (Serial.available()) {
+
+    StaticJsonDocument<300> doc;
+    DeserializationError err = deserializeJson(doc, Serial);
+
+    if (err == DeserializationError::Ok) 
+    {
+     String json;
+    serializeJson(doc, json);;
+
+    const char* srce = doc["source"];
+    const char* msg = doc["msg"];
+    int timestamp = 1000;
+
+    memcpy(theData.sender,srce,13);
+    memcpy(theData.message,msg,100);
+    theData.timestamp = timestamp;
+
+    memcpy(abbrev_message,msg,10);
+    
+     u8x8.clear();
+     u8x8.setCursor(0,0);
+     u8x8.print(srce);
+     u8x8.setCursor(0,2);
+     u8x8.print(abbrev_message);
+
+     rf95.send((uint8_t *)&theData, sizeof(theData));
+
+    Serial.println("Waiting for packet to complete..."); delay(10);
+    rf95.waitPacketSent();
+   Serial.println("Sent."); delay(10);
+
+        
+    } 
+    else 
+    {
+      // Print error to the "debug" serial port
+      Serial.print("deserializeJson() returned ");
+      Serial.println(err.c_str());
+  
+      // Flush all bytes in the "link" serial port buffer
+      while (Serial.available() > 0)
+        Serial.read();
+    }
+    
+  }
+}
+
+int setupLoRa() {
+
+  int errCode=1;
+  
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    errCode = 0;
+    
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    //while (1);
+    errCode = 0;
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+
+  return errCode;
+  
+}
---
+++ arduino/radio_receive_mothbot_struct/radio_receive_mothbot_struct.ino
@@ -0,0 +1,153 @@
+#include <U8x8lib.h>
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <RH_RF95.h>
+#include <ArduinoJson.h> //https://arduinojson.org/v6/doc/installation/
+
+// Radio pins for mothbot
+#define RFM95_CS 8
+#define RFM95_RST 7
+#define RFM95_INT 2
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+// Blinky on receipt
+#define LED 5
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+typedef struct {
+  char sender[13];
+  char message[100]; 
+  int timestamp;
+} Payload;
+Payload theData;
+
+
+
+void setup()
+{
+
+  
+  pinMode(LED, OUTPUT);
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+
+   delay(1000);
+   
+  Serial.begin(9600);
+  //while (!Serial) {
+   // delay(1);
+  //}
+  delay(100);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.clearDisplay();
+
+  testdrawchar2(3);
+  
+  //Serial.println("Feather LoRa RX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+  Serial.print("max buff:");
+  Serial.println(RH_RF95_MAX_MESSAGE_LEN);
+  
+}
+
+void loop()
+{
+  if (rf95.available())
+  {
+    // Should be a message for us now
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+
+    if (rf95.recv(buf, &len))
+    {
+
+ 
+      
+      digitalWrite(LED, HIGH);
+      //RH_RF95::printBuffer("Received: ", buf, len);
+      Serial.print("Got: ");
+      Serial.println((char*)buf);
+       Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
+
+     theData = *(Payload*)
+      Serial.print(" sender=");
+      Serial.print(theData.sender);
+      
+      
+      uint8_t data[] = "And hello back to you";
+      rf95.send(data, sizeof(data));
+      rf95.waitPacketSent();
+      Serial.println("Sent a reply");
+      digitalWrite(LED, LOW);
+      
+      
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+}
+
+void testdrawchar2(int number) {
+  display.clearDisplay();
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+    //display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.println("K30 CO2 Test");
+    display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.println();
+  display.print("PPM: ");
+  display.println(number);
+  display.println();
+  //display.println("MOTHBOT IS GO!");
+    display.display();
+}
