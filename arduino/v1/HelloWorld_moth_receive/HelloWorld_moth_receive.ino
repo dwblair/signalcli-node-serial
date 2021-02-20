abfe3ccd1e34449948c5591480863126ad5fb5a6
Don Blair <donblair@gmail.com>
2021-02-19 22:41:35 -0500

    update

---
+++ arduino/v1/HelloWorld_moth_receive/HelloWorld_moth_receive.ino
@@ -0,0 +1,154 @@
+/*
+
+  HelloWorld.ino
+
+  Universal 8bit Graphics Library (https://github.com/olikraus/u8g2/)
+
+  Copyright (c) 2016, olikraus@gmail.com
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification, 
+  are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this list 
+    of conditions and the following disclaimer.
+    
+  * Redistributions in binary form must reproduce the above copyright notice, this 
+    list of conditions and the following disclaimer in the documentation and/or other 
+    materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+
+*/
+
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+//#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+//#endif
+//#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+//#endif
+
+#include <RH_RF95.h>
+//#include <ArduinoJson.h> //https://arduinojson.org/v6/doc/installation/
+
+// Radio pins for mothbot
+#define RFM95_CS 8
+#define RFM95_RST 7
+#define RFM95_INT 2
+
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);  // Adafruit ESP8266/32u4/ARM Boards + FeatherWing OLED
+
+
+// End of constructor list
+
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+
+typedef struct {
+  char sender[13];
+  char message[40]; 
+  int timestamp;
+} Payload;
+Payload theData;
+
+
+void setup(void) {
+  u8g2.begin();
+
+  Serial.begin(9600);
+
+//pinMode(LED, OUTPUT);
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+  Serial.print("max buff:");
+  Serial.println(RH_RF95_MAX_MESSAGE_LEN);
+
+  
+  u8g2.clearBuffer();          // clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
+  u8g2.drawStr(0,10,"Starting up!");  // write something to the internal memory
+  u8g2.sendBuffer();          // transfer internal memory to the display
+  delay(1000);  
+  
+}
+
+int index = 0;
+
+void loop(void) {
+
+ Serial.println("yea");
+ delay(1000);
+  
+
+ if (rf95.available())
+  {
+
+    // Should be a message for us now
+    uint8_t buf[sizeof(Payload)];
+    uint8_t len = sizeof(buf);
+
+    
+    if (rf95.recv(buf, &len))
+    {
+      Serial.println("hello");
+      theData = *(Payload*)buf;
+
+  
+      Serial.print(" sender=");
+      Serial.print(theData.sender);
+      u8g2.clearBuffer();          // clear the internal memory
+      u8g2.drawStr(0,20,theData.sender);  // write something to the internal memory
+      u8g2.drawStr(0,30,theData.message);  // write something to the internal memory
+      u8g2.sendBuffer();   
+      /*Serial.print("message=");
+      Serial.print(theData.message);
+      */
+    }
+    
+    }
+  }
